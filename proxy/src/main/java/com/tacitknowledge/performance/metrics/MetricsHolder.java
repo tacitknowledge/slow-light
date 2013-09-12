2233fac79aba673bfc78eee246504e2d54c31173
Ivan Frolov <ifrolov@tacitknowledge.com>
2013-09-12 08:28:02 +0300

    Metrics holder DTO.

---
+++ proxy/src/main/java/com/tacitknowledge/performance/metrics/MetricsHolder.java
@@ -0,0 +1,24 @@
+package com.tacitknowledge.performance.metrics;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.netflix.servo.annotations.DataSourceType;
+import com.netflix.servo.annotations.Monitor;
+
+/** @author Ivan Frolov (ifrolov@tacitknowledge.com) */
+public class MetricsHolder
+{
+    @Monitor(name = "counter", type = DataSourceType.COUNTER)
+    public final AtomicInteger counter = new AtomicInteger(0);
+
+    @Monitor(name="time", type = DataSourceType.COUNTER)
+    public final AtomicLong time = new AtomicLong(0);
+
+    @Monitor(name="bytesIn", type = DataSourceType.COUNTER)
+    public final AtomicLong bytesIn = new AtomicLong(0);
+
+    @Monitor(name="bytesOut", type = DataSourceType.COUNTER)
+    public final AtomicLong bytesOut = new AtomicLong(0);
+
+}
