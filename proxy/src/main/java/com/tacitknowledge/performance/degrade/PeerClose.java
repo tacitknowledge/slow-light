eea9f3d2404bbb7f243e43e4fa5392e2b35f9a5a
Ivan Frolov <ifrolov@tacitknowledge.com>
2013-09-12 08:26:49 +0300

    Implement peer close on connection degradation handler.

---
+++ proxy/src/main/java/com/tacitknowledge/performance/degrade/PeerClose.java
@@ -0,0 +1,23 @@
+package com.tacitknowledge.performance.degrade;
+
+import com.tacitknowledge.performance.Component;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.socket.SocketChannel;
+
+/** @author Ivan Frolov (ifrolov@tacitknowledge.com) */
+public class PeerClose implements Component
+{
+    @Override
+    public void initChannel(final SocketChannel ch)
+    {
+        ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+            @Override
+            public void channelActive(final ChannelHandlerContext ctx) throws Exception
+            {
+                ctx.channel().close();
+            }
+        });
+    }
+}
