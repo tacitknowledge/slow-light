c35b9e397d1101a9db75d26a7ed057c00ae61ae6
Alexandr Donciu <adonciu@tacitknowledge.com>
2013-12-26 17:23:11 +0200

    refactored slowlight server main class

--- proxy/src/main/java/com/tacitknowledge/slowlight/proxyserver/server/SlowlightServer.java
+++ proxy/src/main/java/com/tacitknowledge/slowlight/proxyserver/server/SlowlightServer.java
@@ -1,56 +1,66 @@
 package com.tacitknowledge.slowlight.proxyserver.server;
 
-import com.google.gson.Gson;
+import com.tacitknowledge.slowlight.proxyserver.config.ConfigBuilder;
 import com.tacitknowledge.slowlight.proxyserver.config.ServerConfig;
 import com.tacitknowledge.slowlight.proxyserver.config.SlowlightConfig;
+import com.tacitknowledge.slowlight.proxyserver.config.json.JSONConfigBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.lang.reflect.InvocationTargetException;
 
 /** @author Alexandr Donciu (adonciu@tacitknowledge.com) */
 public class SlowlightServer
 {
     private static final Logger LOG = LoggerFactory.getLogger(SlowlightServer.class);
 
-    public static void main(String[] args) throws FileNotFoundException
+    private ConfigBuilder configBuilder;
+
+    public static void main(String[] args) throws Exception
     {
-        if (args.length < 1)
-        {
-            throw new IllegalArgumentException("Usage: " + SlowlightServer.class.getSimpleName() + " <config_file>");
-        }
+        final SlowlightServer slowlightServer = new SlowlightServer();
+        slowlightServer.setConfigBuilder(new JSONConfigBuilder());
 
-        final SlowlightConfig config = loadConfig(args[0]);
+        slowlightServer.start(args);
+    }
 
+    public void start(final String[] args) throws Exception
+    {
+        validateArguments(args);
+
+        final SlowlightConfig config = configBuilder.getConfig(args[0]);
         for (ServerConfig serverConfig : config.getServers())
         {
             try
             {
-                final Class<?> serverClass = Class.forName(config.getServerTypes().get(serverConfig.getType()));
-                final Server server  = (Server) serverClass.getConstructor(ServerConfig.class).newInstance(serverConfig);
+                final Server server = instantiateServer(config, serverConfig);
                 server.start();
             }
             catch (final Exception e)
             {
                 LOG.error("Cannot create server [{}]", serverConfig.getId(), e);
+                throw e;
             }
         }
     }
 
-    private static SlowlightConfig loadConfig(final String configFileName) throws FileNotFoundException
+    protected Server instantiateServer(final SlowlightConfig config, final ServerConfig serverConfig)
+            throws ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException
     {
-        final File configFile = new File(configFileName);
+        final Class<?> serverClass = Class.forName(config.getServerTypes().get(serverConfig.getType()));
+        return (Server) serverClass.getConstructor(ServerConfig.class).newInstance(serverConfig);
+    }
 
-        try
+    private void validateArguments(final String[] args)
+    {
+        if (args.length < 1)
         {
-            final String configJson = new Scanner(configFile).useDelimiter("\\Z").next();
-            return new Gson().fromJson(configJson, SlowlightConfig.class);
+            throw new IllegalArgumentException("Usage: " + SlowlightServer.class.getSimpleName() + " <config_file>");
         }
-        catch (final FileNotFoundException e)
-        {
-            throw new IllegalArgumentException("Cannot find config file [" + configFileName + "]");
-        }
+    }
+
+    public void setConfigBuilder(final ConfigBuilder configBuilder)
+    {
+        this.configBuilder = configBuilder;
     }
 }
