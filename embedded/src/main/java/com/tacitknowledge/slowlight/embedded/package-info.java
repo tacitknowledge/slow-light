548dfca372c3147780ffd19fc9c6a2480e4af7e7
Alexandr Donciu <adonciu@tacitknowledge.com>
2014-01-13 12:30:58 +0200

    package info update

--- embedded/src/main/java/com/tacitknowledge/slowlight/embedded/package-info.java
+++ embedded/src/main/java/com/tacitknowledge/slowlight/embedded/package-info.java
@@ -6,21 +6,21 @@
  * failures under concurrent load.
  *
  *
- * The DegradationHandler uses the strategy and a threadpool to submit Callables to the threadpool.  These
+ * The DegradationProxyHandler uses the strategy and a threadpool to submit Callables to the threadpool.  These
  * DegradationCallables put the thread pool threads to sleep for some amount of time, generally calculated based on
  * current thread pool utilization and the min/max latency of the DegradationStrategy.  If the sleep time exceeds a certain
  * threshold (pass rate), the calls eventually will be overriden with a configured error object or throw an exception to
  * simulate a failure.
  *
- * @see DegradationHandler
- * @see DegradationCallable
- * @see DegradationStrategy
- * @see DegradationPlan - a DTO like object containing results of applying specific strategy rules for a specific call
+ * @see com.tacitknowledge.slowlight.embedded.DegradationHandler
+ * @see com.tacitknowledge.slowlight.embedded.DegradationCallable
+ * @see com.tacitknowledge.slowlight.embedded.DegradationStrategy
+ * @see com.tacitknowledge.slowlight.embedded.DegradationPlan - a DTO like object containing results of applying specific strategy rules for a specific call
  *
  * The DegradationStrategy determines configs for min and max latency via service demand and timeout.  It also allows
  * configuration of errors, failure rates, and throwables.
  *
- * @see DefaultDegradationStrategy
+ * @see com.tacitknowledge.slowlight.embedded.DefaultDegradationStrategy
  *
  */
 package com.tacitknowledge.slowlight.embedded;
