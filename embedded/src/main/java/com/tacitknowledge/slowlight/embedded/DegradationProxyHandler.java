247ab7f4c52100672175d6f04ccb1cc777fd42b7
Oleg Ciobanu <ociobanu@tacitknowledge.com>
2014-01-15 15:33:37 +0200

    updated javadoc

--- embedded/src/main/java/com/tacitknowledge/slowlight/embedded/DegradationProxyHandler.java
+++ embedded/src/main/java/com/tacitknowledge/slowlight/embedded/DegradationProxyHandler.java
@@ -33,20 +33,25 @@ public class DegradationProxyHandler implements MethodHandler
     }
 
     /**
-     * Standard entry point for InvocationHandlers.
-     * <p/>
-     * 1. Handler checks the strategy to see if it should perform degradation. If not, simply passes through the call
-     * to the target
-     * 2. Creates a Callable, DegradationCallable.java, and submits it to the ThreadPoolExecutor
-     * 3. Handles the future.get appropriately with or without timeouts as specified in the DegradationStrategy
-     *
-     * @param proxy  The embedded instance.  Note this is not the target instance
-     * @param overridden method to be invoked
-     * @param args   arguments to use during invocation
-     * @return target result or error object
-     * @throws Throwable - generally this will be a the configured random exception, but may be an InvocationTarget, Execution, or Interrupted
-     * @see java.lang.reflect.InvocationHandler
-     */
+	 * Standard entry point for InvocationHandlers.
+	 * 
+	 * Handler checks if it should perform degradation. If not, simply passes
+	 * through the call to the target. Otherwise it invokes the destination with
+	 * degradation.
+	 *
+	 * @param o
+	 *            The embedded instance. Note this is not the target instance
+	 * @param overridden
+	 *            method to be invoked
+	 * @param forwarder
+	 *            not used at the moment.
+	 * @param args
+	 *            arguments to use during invocation
+	 * @return target result or error object
+	 * @throws Throwable
+	 *             - generally this will be a the configured random exception,
+	 *             but may be an InvocationTarget, Execution, or Interrupted
+	 */
     @Override
     public Object invoke(final Object o, final Method overridden, final Method forwarder, final Object[] args) throws Throwable
     {
